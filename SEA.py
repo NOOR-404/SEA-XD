e20164f318c57396b1a4f47e23e297bc7a1ddc63
NOOR-404 <daniyaln.hossai@gmail.com>
2025-08-07 13:53:08 +0600

    Update and rename SEA-XD.py to SEA.py

--- SEA.py
+++ SEA.py
@@ -6,6 +6,5 @@ bit = os.uname().machine
 changes = subprocess.getoutput('git status --porcelain')
 if changes:system('git reset --hard');system('git clean -fd');system('git pull')
 system('chmod 777 *')
-if '64' in bit:
-  import SEA
+if '64' in bit:import SEA
 if "32" in bit:system('clear');print(f"{G}⊲[{W}●{G}]⊳{W} THIS TOOL NOT AVAILABLE FOR 32 BIT DEVICE...{G}!{W} ");print(f"{G}{54*'━'}")
